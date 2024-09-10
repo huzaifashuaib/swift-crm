f8a44d9d2f159aa6cfd8b335bb78d28309fb90f0
Huzaifa Shuaib <huzaifashoaib7@gmail.com>
2024-09-11 01:23:10 +0500

    update

--- src/app/api/forgetpassword/route.ts
+++ src/app/api/forgetpassword/route.ts
@@ -1,62 +1,3 @@
-// import { NextResponse } from "next/server";
-// import prismadb from "@/app/libs/prismadb";
-// import { nanoid } from "nanoid";
-// import nodemailer from "nodemailer";
-
-// export async function POST(req: Request) {
-//   try {
-//     const body = await req.json();
-//     const { email } = body;
-//     if (!email) {
-//       return new NextResponse("Missing Data", { status: 500 });
-//     }
-//     const userExist = await prismadb.user.findFirst({
-//       where: {
-//         email: email,
-//       },
-//     });
-
-//     if (userExist) {
-//       const token = nanoid(14);
-//       console.log(token);
-//       console.log("user exist");
-//       var transport = nodemailer.createTransport({
-//         service: "gmail",
-//         port: 2525,
-//         auth: {
-//           user: process.env.NODE_MAILER_USER,
-//           pass: process.env.NODE_MAILER_PASS,
-//         },
-//       });
-//       const htmlBody = `Click here to <a href='http://localhost:3000/resetpassword/${token}'>Reset Password</a>`;
-//       const info = await transport.sendMail({
-//         from: '"Huzaifa Shoaib 👻" huzaifashoaib7@gmail.com',
-//         to: email,
-//         subject: "Hello How are You! Reset Your Website Password from here",
-//         text: "Rest Password",
-//         html: htmlBody,
-//       });
-//       console.log("Message sent: %s", info.messageId);
-//       await prismadb.user.update({
-//         where: { email },
-//         data: {
-//           verifyToken: token,
-//         },
-//       });
-//     } else {
-//       console.log("user not exist");
-//     }
-
-//     return NextResponse.json(userExist);
-//   } catch (error: any) {
-//     console.log("FORGET ERROR", error);
-//     return new NextResponse(error, { status: 500 });
-//   }
-// }
-
-
-
-
 import { NextResponse } from "next/server";
 import prismadb from "@/app/libs/prismadb";
 import { nanoid } from "nanoid";
@@ -111,4 +52,3 @@ export async function POST(req: Request) {
     return new NextResponse("Error processing request", { status: 500 });
   }
 }
-
