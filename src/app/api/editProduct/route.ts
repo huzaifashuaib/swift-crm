0ab2b59c34e02fb9333b6859c6aedf78f5c1f9db
Huzaifa Shuaib <huzaifashoaib7@gmail.com>
2024-09-11 20:36:01 +0500

    change post method to put

--- src/app/api/editProduct/route.ts
+++ src/app/api/editProduct/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
 import prismadb from "@/app/libs/prismadb";
-import axios from "axios";
 
-export async function POST(req: Request) {
+export async function PUT(req: Request) {
   try {
     const body = await req.json();
     const { id, name, price, quantity, category, description,imgUrl,publicId } = body;
@@ -12,18 +11,7 @@ export async function POST(req: Request) {
     }
 
     if (name || price || quantity || category || description || imgUrl || publicId) {
-      // if (publicId) {
-      //   const productPublicId=publicId;
-      //   const response = await axios.post(
-      //     "https://swift-crm-techloset.vercel.app/api/removeImageCloud",
-      //     { productPublicId }
-      //   );
   
-      //   if (response.status !== 200) {
-      //     throw new Error(`Failed to remove image: ${response.statusText}`);
-      //   }
-      // }
-
       const updatedProduct = await prismadb.product.update({
         where: { id },
         data: {
